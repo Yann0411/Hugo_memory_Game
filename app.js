67fc211b46e467dae2d59f608461ad556a7efe44
Yann Christophe <77779939+Yann0411@users.noreply.github.com>
2024-02-29 22:54:26 +0100

    compteur_a_mettre

---
+++ app.js
@@ -0,0 +1,90 @@
+const cards = [
+    'https://picsum.photos/id/237/100/100',
+    'https://picsum.photos/id/238/100/100',
+    'https://picsum.photos/id/239/100/100',
+    'https://picsum.photos/id/240/100/100',
+    'https://picsum.photos/id/241/100/100',
+    'https://picsum.photos/id/242/100/100',
+    'https://picsum.photos/id/243/100/100',
+    'https://picsum.photos/id/244/100/100'
+];
+
+const appBoard = document.getElementById('appBoard')
+
+function createCard(CardUrl) {
+
+
+    const divImg = document.createElement('div')
+    divImg.classList.add('img')
+    divImg.dataset.value = CardUrl
+
+
+    const img = document.createElement('img')
+    img.classList.add('card-content')
+    img.src = CardUrl
+    img.alt = "image"
+
+    divImg.appendChild(img)
+    divImg.addEventListener('click', handleClick)
+
+
+    return divImg
+
+}
+
+let allcardsArray = []
+
+
+function shuffleArray(arrayToshuffle) {
+    const arrayShuffled = arrayToshuffle.sort(() => 0.5 - Math.random());
+    return arrayShuffled;
+}
+
+function duplicatedArray(originArray) {
+    allcardsArray.push(...originArray)
+    allcardsArray.push(...originArray)
+    return allcardsArray
+
+}
+
+let allcard = duplicatedArray(cards)
+
+
+allcard = shuffleArray(allcard)
+
+allcard.forEach((card) => {
+    htmlCard = createCard(card)
+    appBoard.appendChild(htmlCard)
+
+})
+let compteur = 0
+let matchedCard = []
+
+function handleClick(e) {
+    const divElm = e.target.parentElement
+    divElm.classList.add('flip')
+ 
+    matchedCard.push(divElm)
+   setTimeout(() => {
+    if (matchedCard.length == 2) {
+        console.log(matchedCard[0])
+        console.log( matchedCard[1] )
+        if (matchedCard[0].dataset.value == matchedCard[1].dataset.value ) {
+            matchedCard[0].classList.add("matched");
+            matchedCard[1].classList.add("matched");
+            matchedCard[0].removeEventListener('click', handleClick);
+            matchedCard[1].removeEventListener('click', handleClick);
+        }
+        else {
+            compteur++
+            matchedCard[0].classList.remove("flip");
+            matchedCard[1].classList.remove("flip");
+        }
+        matchedCard = []
+        console.log(compteur)
+    }
+    
+   }, 1000);
+  
+}
+
